40ffe45321d7d588e3b3eeae18282133204ffb52
Touchet <gtouchet@myges.fr>
2021-10-30 01:14:48 +0200

    Json reader
    Added a json reader class to fetch data

--- src/main/java/esgi/al/App.java
+++ src/main/java/esgi/al/App.java
@@ -1,47 +1,28 @@
 package esgi.al;
 
 import esgi.al.controllers.UserController;
-import esgi.al.enumerators.PaymentMethod;
-import esgi.al.enumerators.StreetType;
 import esgi.al.exceptions.FailedToCreateUser;
-import esgi.al.exceptions.NoUserFound;
-import esgi.al.models.Address;
 import esgi.al.models.User;
 import esgi.al.models.Users;
 import esgi.al.repositories.SQLUsers;
 import esgi.al.services.UserService;
-import esgi.al.utils.Uuid;
+import esgi.al.utils.JsonReader;
+import java.util.List;
 
 public class App
 {
-    public static void main(String[] args) throws FailedToCreateUser, NoUserFound
+    public static void main(String[] args) throws FailedToCreateUser
     {
         final Users users = new SQLUsers();
         final UserService userService = new UserService(users);
         final UserController userController = new UserController(userService);
 
-        User user = User.of(
-                Uuid.generate(),
-                "GTouchet",
-                "Abcd1234!",
-                "Touchet",
-                Address.of(
-                        "Draveil",
-                        StreetType.rue,
-                        "Pierre Brossolette",
-                        134
-                ),
-                PaymentMethod.CARD
-        );
-
-        // We create the user, using the user controller
-        userController.createUser(user);
-
-        // We get the newly created user by it's ID
-        System.out.println(userController.getById(user.getId()));
-
-        // And here we DON'T find the newly user, throwing a NoUserFound exception
-        // Unless we are very very VERY lucky !
-        System.out.println(userController.getById(Uuid.generate()));
+        List<User> jsonUsers = JsonReader.getUserDataFrom("./res/registeredUsers.json");
+        /*for (User jsonUser : jsonUsers)
+        {
+            System.out.println(jsonUser);
+            userController.createUser(jsonUser);
+        }*/
+        //System.out.println(userController.getAll());
     }
 }
